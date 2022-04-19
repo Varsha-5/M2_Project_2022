a8faf9158f89e3ad02ac0091f9728d3695e544c9
Varsha-5 <101313943+Varsha-5@users.noreply.github.com>
2022-04-19 12:41:03 +0530

    Create PWM.c

---
+++ src/PWM.c
@@ -0,0 +1,41 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+uint8_t PWM(uint16_t adc_value)
+{
+    DDRB |= (1 << PB1);  ///<Set PB1 bit in DDRB
+
+    
+    TCCR1A |= (1 << COM1A1) | (1 << WGM11) | (1 << WGM10);
+    TCCR1B |= (1 << CS11) | (1 << CS10);
+    uint8_t temperature = 0;
+
+    if(adc_value >= 0 && adc_value <= 200)
+    {
+        OCR1A = 200;   ///<20% dutycycle
+        temperature = 20;
+        _delay_ms(200);
+    }
+    else if(adc_value >= 210 && adc_value <= 500)
+    {
+        OCR1A = 400;   ///<40% dutycycle
+        temperature = 25;
+        _delay_ms(200);
+    }
+    else if(adc_value >= 510 && adc_value <= 700)
+    {
+        OCR1A = 700;   ///<70% dutycycle
+        temperatute = 29;
+        _delay_ms(200);
+    }
+    else if(adc_value >= 710 && adc_value <= 1024)
+    {
+        OCR1A = 900;   ///<95% dutycycle
+        temperature = 33;
+        _delay_ms(200);
+    }
+    else
+        OCR1A = 0;   ///<0% dutycycle
+
+    return temperature;
+}
