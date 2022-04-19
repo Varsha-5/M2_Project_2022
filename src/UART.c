9fadc259e42e83db941b7dd9e0e8416cf927bdb9
Varsha-5 <101313943+Varsha-5@users.noreply.github.com>
2022-04-19 12:43:02 +0530

    Create UART.c

---
+++ src/UART.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "Activity4.h"
+
+void UART_init(uint16_t ubrr_value)
+{
+    BAUD_LOW; // Set Baud rate low
+    BAUD_HIGH;// Set Baud rate high
+    SET_CHAR; // Set data bits
+    ENABLE_Rx_Tx; // Enable Transmitter and Receiver
+}
+
+
+void UART_write(char val)
+{
+    while(WAIT_Tx)
+    {
+        //Do nothing
+    }
+    DATA_ReadWrite=val;
+}
