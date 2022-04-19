af7b160a7ef0b9e90185a183776f168cf8a810b4
Varsha-5 <101313943+Varsha-5@users.noreply.github.com>
2022-04-19 14:40:21 +0530

    Create GPIO.h

---
+++ inc/GPIO.h
@@ -0,0 +1,13 @@
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+void Led_Actuator(void);
+
+///Variable to indicate the seat Check
+volatile unsigned Seat_Check;
+
+///Variable to indicate the switch Check
+volatile unsigned Switch_Check;
+
+
+#endif 
