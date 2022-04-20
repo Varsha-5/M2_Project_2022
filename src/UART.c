7dada468a9acaf6898ab29f5f4c3656a09443466
Varsha-5 <101313943+Varsha-5@users.noreply.github.com>
2022-04-20 10:47:10 +0530

    Update UART.c

--- src/UART.c
+++ src/UART.c
@@ -1,21 +1,32 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "UART.h"
+#define BAUD 9600
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)  ///<Baudrate calculation
 
-void UART_init(uint16_t ubrr_value)
+void USARTInit(void)
 {
-    BAUD_LOW; // Set Baud rate low
-    BAUD_HIGH;// Set Baud rate high
-    SET_CHAR; // Set data bits
-    ENABLE_Rx_Tx; // Enable Transmitter and Receiver
+    ///Set baudrate
+    UBRR0H |= (BAUDRATE >> 8);
+    UBRR0L |= BAUDRATE;
+
+    ///Set character size to 8 bit
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0) | (1 << TXCIE0);
 }
 
-
-void UART_write(char val)
+char USARTReadChar(void)
 {
-    while(WAIT_Tx)
-    {
-        //Do nothing
-    }
-    DATA_ReadWrite=val;
+    ///Wait to receive data
+    while(!(UCSR0A & (1 << RXC0)));
+
+    return UDR0;
+}
+
+void USARTWriteChar(char value)
+{
+    ///Wait to transmitt data
+    while(!(UCSR0A & (1 << UDRE0)));
+
+    UDR0 = value;
 }
